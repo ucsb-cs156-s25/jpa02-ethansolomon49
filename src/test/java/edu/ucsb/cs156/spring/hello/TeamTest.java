4c7c82a4f5045e23afd1524b4f8cd8e6187ad5c3
Ethan Solomon <ethansolomon@ucsb.edu>
2025-04-04 01:22:12 -0700

    tests for team

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,62 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_returns_true_for_same_object() {
+        assert(team.equals(team));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_object() {
+        String str = "test";
+        assert(!team.equals(str));
+    }
+
+    @Test
+    public void equals_returns_true_for_same_team() {
+        team.addMember("Ethan S");
+        team.addMember("Fahim Z");
+
+        Team team2 = new Team("test-team");
+        team2.addMember("Ethan S");
+        team2.addMember("Fahim Z");
+        assert(team.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_team_members() {
+        team.addMember("Ethan S");
+        team.addMember("Fahim Z");
+
+        Team team2 = new Team("test-team");
+        team2.addMember("Ethan S");
+        team2.addMember("Fahim Z");
+        team2.addMember("Jeremiah W");
+        assert(!team.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_team_name() {
+        team.addMember("Ethan S");
+        team.addMember("Fahim Z");
+
+        Team team2 = new Team("test-team-2");
+        team2.addMember("Ethan S");
+        team2.addMember("Fahim Z");
+        assert(!team.equals(team2));
+    }
+
+    @Test
+    public void hashCode_values() {
+        // instantiate t as a Team object
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+    }
 
 }
